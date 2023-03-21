d04a7686bc811d4fc2a65e7be02fa4adc534d593
Mihail Donchev <mihaildonchev88@abv.bg>
2023-03-21 13:09:00 +0200

    Change the hours

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ function displayTime() {
 
     seconds = seconds < 10 ? `0${seconds}` : seconds;
     minutes = minutes < 10 ? `0${minutes}` : minutes;
-    hours = hours < 10 ? `0${hours}` : hours;
     days = days < 10 ? `0${days}` : days;
     months = months < 10 ? `0${months}` : months;
     meridian.textContent = hours >= 12 ? meridian.textContent = 'PM' : meridian.textContent = 'AM';
 
     if (hours > 12) {
         hours = hours - 12;
+        hours = hours < 10 ? `0${hours}` : hours;
     }
 
     document.getElementById('hours').innerHTML = hours;
