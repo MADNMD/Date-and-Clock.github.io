701008735e72be8c8528ac60c44962bbe91facb3
Mihail Donchev <mihaildonchev88@abv.bg>
2023-03-21 12:48:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,32 +9,22 @@ function displayTime() {
     let year = dateTime.getFullYear();
     const meridian = document.getElementById('meridian');
 
-    if (hours >= 12) {
-        meridian.textContent = 'PM';
-    } else {
-        meridian.textContent = 'AM';
-    }
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    hours = hours < 10 ? `0${hours}` : hours;
+    days = days < 10 ? `0${days}` : days;
+    months = months < 10 ? `0${months}` : months;
+    meridian.textContent = hours >= 12 ? meridian.textContent = 'PM' : meridian.textContent = 'AM';
 
     if (hours > 12) {
         hours = hours - 12;
     }
     
-        if(months > 9){
-        document.getElementById('months').innerHTML = months;
-    }else{
-        document.getElementById('months').innerHTML = '0' + months;
-    }
-
-    if(months > 9){
-        document.getElementById('months').innerHTML = months;
-    }else{
-        document.getElementById('months').innerHTML = '0' + months;
-    }
-
     document.getElementById('hours').innerHTML = hours;
     document.getElementById('minutes').innerHTML = minutes;
     document.getElementById('seconds').innerHTML = seconds;
     document.getElementById('days').innerHTML = days;
+    document.getElementById('months').innerHTML = months;
     document.getElementById('year').innerHTML = year;
 }
 setInterval(displayTime, 10);
